8803b6c65f8e521c2b0636d091c96972c0b31023
neyoungandy <>
2025-03-10 13:27:45 +0100

    Changes

--- server.js
+++ server.js
@@ -9,22 +9,19 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
-
+const port = process.env.PORT || 3000  // Added fallback value of 3000
+const host = process.env.HOST || 'localhost'
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on port ${port}`)
 })
