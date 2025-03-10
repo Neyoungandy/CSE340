6fd0907f6c6b9513521157bf2f0088f9770f6f3c
neyoungandy <>
2025-03-10 13:40:46 +0100

    New Changes

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+
+// Fix paths by using path.join() for proper directory resolution
+router.use("/css", express.static(path.join(__dirname, "../public/css")));
+router.use("/js", express.static(path.join(__dirname, "../public/js")));
+router.use("/images", express.static(path.join(__dirname, "../public/images")));
+
+// Add a route handler for the root path
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = router;
-
-
-
