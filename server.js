8220aed1f5db8a0064b8e03c4cf3f7697675dd02
neyoungandy <>
2025-03-26 00:44:49 +0100

    server.js updated

--- server.js
+++ server.js
@@ -6,22 +6,112 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
+const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+// Use body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Use cookie parser
+app.use(cookieParser())
+
+// Use token validation
+app.use(utilities.checkJWTToken)
+
+/* ***********************
+ * View Engine and Template
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+// static route
+app.use(require("./routes/static"))
+
+// index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// inventory route
+app.use("/inv", require("./routes/inventoryRoute"))
+
+// account route
+app.use("/account", require("./routes/accountRoute"))
+
+// inbox route
+app.use("/inbox", require("./routes/messageRoute"))
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({
+    status: 404, 
+    message: 'This is not the page you are looking for... Go about your business... Move along...'
+  })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404) {message = err.message} else {message = 'Oops, looks like something went wrong! Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav,
+  })
+})
+
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 3000  // Added fallback value of 3000
-const host = process.env.HOST || 'localhost'
+const port = process.env.PORT
+const host = process.env.HOST
+
 /* ***********************
  * Log statement to confirm server operation
  *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
