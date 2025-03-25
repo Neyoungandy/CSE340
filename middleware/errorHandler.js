ea570199c56a74bfc9a9062976bd7573cf5ebdfa
neyoungandy <>
2025-03-25 23:30:59 +0100

    changed to lower case

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,5 @@
-const errorHandler = (err, req, res, next) => {
+const errorhandler = (err, req, res, next) => {
     console.error(err.stack);
     res.status(err.status || 500).render('errors/500', { message: "Something went wrong!" });
 };
-
-module.exports = errorHandler;
+module.exports = errorhandler;
